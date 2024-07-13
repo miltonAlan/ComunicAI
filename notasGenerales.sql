4881e1c0ea43669a42f4f52bc1be9307f0d6ab21
mpaucar <miltonpaucar99@gmail.com>
2024-07-12 22:26:40 -0500

    configuraciones iniciales

---
+++ notasGenerales.sql
@@ -0,0 +1,18 @@
+Requerimientos:
+- Crear cuenta en GitHub
+    - Usuarios:
+        - codingWhileAlive
+        - 
+        -
+    Software:
+        - VS Code (ultima version)
+        - Git (ultima version)
+        - Node.js (escoger la version LTS)
+        - Android Studio (ultima version 12 jul 2024 )
+            - Android Studio Koala | 2024.1.1
+        - Extension para abrir emulador desde vs code
+            - https://www.youtube.com/watch?v=as_HHSFT304&ab_channel=DeepDiveDevs
+------------------------------------------------------------------------------------
+- Prueba de ambiente antes de desarrollo
+    - https://www.youtube.com/watch?v=T-fhSE8n2CA
+------------------------------------------------------------------------------------
