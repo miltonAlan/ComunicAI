40789e7b2091f1a99f731aecb4871b7cca4f7196
mpaucar <miltonpaucar99@gmail.com>
2024-07-13 21:49:42 -0500

    notas generales: ambiente preparacion

--- notasGenerales.sql
+++ notasGenerales.sql
@@ -16,3 +16,24 @@ Requerimientos:
 - Prueba de ambiente antes de desarrollo
     - https://www.youtube.com/watch?v=T-fhSE8n2CA
 ------------------------------------------------------------------------------------
+Preparacion ambiente definitivo
+------------------------------------------------------------------------------------
+configurar variable:
+    - ANDROID_HOME
+version java
+    - 17
+instalar gradle (Gradle 8.9)
+    - https://gradle.org/install/
+
+Ejecucion proyecto
+    - activar micro en el emulador
+    - dependencias
+        - yarn install
+        - npx expo prebuild -p android
+        - npx expo run:android
+
+creacion apk con EAS
+https://www.youtube.com/watch?v=FBv4PrW5wqY&t=665s&ab_channel=CodewithBetoenEspa%C3%B1ol
+- npm install -g eas-cli
+- eas build:configure
+- eas build -p android --profile preview
