c2b31559ef40ff526b6ac2f674930cc1a1a763f9
mpaucar <miltonpaucar99@gmail.com>
2024-07-17 23:20:50 -0500

    maqueta pantalla funcionalidad principal y consumo de api que lista idiomas

--- front/App.tsx
+++ front/App.tsx
@@ -1,139 +1,3 @@
-/*
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from "react";
-import {Image, Pressable, StyleSheet, Text, View, Button } from "react-native";
-import * as FileSystem from "expo-file-system";
-import { useVoiceRecognition } from './src/hooks/useVoiceRecognition';
-import Tts from 'react-native-tts'; 
-
-export default function App() {
-  const { state, startRecognizing, stopRecognizing, destroyRecognizer } =
-  useVoiceRecognition();
-  const [borderColor, setBorderColor] = useState<"lightgray" | "lightgreen">(
-    "lightgray"
-  );
-  const handleSpeakResults = () => { 
-    // Unir los resultados en una sola cadena--- prueba de guardar en rama
-    const textToSpeak = state.results[0];
-    // Verificar si hay resultados para evitar errores
-    if (textToSpeak.trim().length > 0) {
-      Tts.speak(textToSpeak);
-    }
-  };
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 30 }}>
-        Talk GPT 🤖
-      </Text>
-      <Text>
-        Press and hold this button to record your voice.
-      </Text>
-      <Pressable
-        onPressIn={() => {
-          setBorderColor("lightgreen");
-          startRecognizing();
-
-        }}
-        onPressOut={() => {
-          setBorderColor("lightgray");
-          stopRecognizing();
-        }}
-        style={{
-          width: "90%",
-          padding: 30,
-          gap: 10,
-          borderWidth: 3,
-          alignItems: "center",
-          borderRadius: 10,
-          borderColor: borderColor,
-        }}
-      >
-        <Image style={styles.button} source={require("./assets/button.png")} />
-      </Pressable>
-      <Pressable
-        onPress={handleSpeakResults}
-        style={{
-          marginTop: 20,
-          padding: 10,
-          backgroundColor: "#6495ED",
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ color: "white", fontSize: 18 }}>Speak Results</Text>
-      </Pressable>
-      <Text style={{marginVertical: 10, fontSize: 20}}>
-        {JSON.stringify(state, null, 2)}
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    padding: 20,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  action: {
-    textAlign: "center",
-    color: "#0000FF",
-    marginVertical: 5,
-    fontWeight: "bold",
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-    fontSize: 12,
-  },
-  stat: {
-    textAlign: "center",
-    color: "#B0171F",
-    marginBottom: 1,
-  },
-});
-
-
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-// Importa tus pantallas
-import Untitled from './src/screens/Untitled';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Idiomas from './src/screens/Idiomas'; // Asume que tienes una pantalla Idiomas
-import LoginScreen_2 from './src/screens/LoginScreen_2';
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    
-      <NavigationContainer>
-        <Untitled/>
-      </NavigationContainer>
-    
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen_2} />
-        <Stack.Screen name="Idiomas" component={Idiomas} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-*/
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -141,6 +5,7 @@ import Idiomas from './src/screens/Idiomas';
 import Untitled from './src/screens/Untitled';
 import LoginScreen_2 from './src/screens/LoginScreen_2';
 import SignUpScreen from './src/screens/SignUpScreen_2.1';
+import Interpreter from './src/screens/Interpreter_3';
 
 const Stack = createStackNavigator();
 
@@ -151,7 +16,8 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen_2} />
         <Stack.Screen name="Idiomas" component={Idiomas} />
         <Stack.Screen name="Untitled" component={Untitled} />
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen}/>
+        <Stack.Screen name="Interpreter" component={Interpreter}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
